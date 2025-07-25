32d76d1b19c3929463f3aebf038a4643467eda03
Abhishek Gupta <shabhishekgupta890@gmail.com>
2025-07-25 23:59:40 +0530

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -28,8 +28,8 @@ export const signup = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             maxAge: 7 * 24 * 60 * 60 * 1000,
-            sameSite: "lax",
-            secure: false
+            sameSite: "None",
+            secure: true
         });
 
         return res.status(201).json(user);
@@ -58,8 +58,8 @@ export const Login = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             maxAge: 7 * 24 * 60 * 60 * 1000,
-            sameSite: "lax",
-            secure: false
+            sameSite: "None",
+            secure: true
         });
 
         return res.status(200).json(user);
