90e5996fd3ce61a24ed8b4d91329093d5ff247b3
Abhishek Gupta <shabhishekgupta890@gmail.com>
2025-07-25 23:57:52 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ import geminiResponse from './gemini.js'
 
 const app = express()
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:8000"],
+    origin: ["https://assistant-vdqz.onrender.com"],
     credentials: true
 }));
 app.use(express.json())
